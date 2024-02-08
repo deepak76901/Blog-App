305feb67780b0d54e10827be7f8358f0142c8a34
deepak76901 <deepakb76901@gmail.com>
2024-02-08 18:12:28 +0530

    signup UI and functionality added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Button, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import {FaMoon} from "react-icons/fa"
+import { FaMoon } from "react-icons/fa";
 
 export default function Header() {
   const path = useLocation().pathname;
@@ -12,9 +12,8 @@ export default function Header() {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-600 to-red-500 rounded-md text-white">
-          Tech
+          CuriousCanvas
         </span>
-        Blog
       </Link>
       <form>
         <TextInput
@@ -25,32 +24,28 @@ export default function Header() {
         />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-        <AiOutlineSearch/>
+        <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
         <Button className="w-12 h-10 hidden sm:inline " color="gray" pill>
-          <FaMoon/>
+          <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline >Sign In</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>
+            Sign In
+          </Button>
         </Link>
-        <Navbar.Toggle/>
+        <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path==="/"} as={"div"}>
-          <Link to="/">
-            Home
-          </Link>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path==="/about"} as={"div"}>
-          <Link to="/about">
-            About
-          </Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path==="/projects"} as={"div"}>
-          <Link to="/projects">
-            Projects
-          </Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
