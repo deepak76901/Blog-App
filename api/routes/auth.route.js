c6b997ac3573eeab0e1f2cceeaf17f4bc7eac39c
deepak76901 <deepakb76901@gmail.com>
2024-02-11 14:27:04 +0530

    add OAuth Functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { signIn, signUp } from "../controllers/auth.controller.js";
+import { google, signIn, signUp } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/signup", signUp).post("/signin", signIn);
+router.post("/signup", signUp).post("/signin", signIn).post("/google",google);
 
 export default router;
