f7c3d383443a0c843f550273ebaa301b9e4a6e39
deepak76901 <deepakb76901@gmail.com>
2024-02-18 20:26:12 +0530

    working on dashboard

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -4,7 +4,7 @@ export default function ThemeProvider({children}) {
     const {theme} = useSelector(state => state.theme)
   return (
     <div className={theme}>
-        <div className="text-black bg-white dark:text-white dark:bg-gray-800 min-h-screen">
+        <div className="text-black bg-white dark:text-white dark:bg-gray-900 min-h-screen">
             {children}
         </div>
     </div>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    console.log(tabFromUrl)
+    if(tabFromUrl){
+      setTab(tabFromUrl)
+    }
+  }, [location.search]);
+
   return (
-    <div>
-      Dashboard
+    <div className="min-h-screen flex flex-col sm:flex-row" >
+      {/* SideBar */}
+      <div>
+        <DashSidebar />
+      </div>
+      {/* Profile */}
+      <div>
+        {tab === "profile" && <DashProfile />}
+      </div>
     </div>
-  )
+  );
 }
